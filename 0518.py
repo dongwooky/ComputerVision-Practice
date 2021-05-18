9b477ef075573d5e5b57e3c3a139696c27988af9
Dongwook Kwon <tomiyadragon@gmail.com>
2021-05-18 23:59:34 +0900

    Create 0518.py

---
+++ 0518.py
@@ -0,0 +1,21 @@
+import sys
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print('camera open failed!')
+    sys.exit()
+    
+while True:
+    ret, frame = cap.read()
+    
+    if not ret:
+        break
+    
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(20) == 27:
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
