4ed43d9cb46785508542f3edada7976fd5c34b08
Dongwook Kwon <tomiyadragon@gmail.com>
2021-05-19 02:12:46 +0900

    Update 0518.py

--- 0518.py
+++ 0518.py
@@ -1,11 +1,15 @@
 import sys
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('video1.mp4')
 
 if not cap.isOpened():
-    print('camera open failed!')
+    print('video open failed!')
     sys.exit()
+    
+w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print(w, h)
     
 while True:
     ret, frame = cap.read()
@@ -13,7 +17,10 @@ while True:
     if not ret:
         break
     
+    edge = cv2.Canny(frame, 50, 150)
+    
     cv2.imshow('frame', frame)
+    cv2.imshow('edge', edge)
     if cv2.waitKey(20) == 27:
         break
     
