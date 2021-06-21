1433b082a6b78d9f96280544d9bb93a17306d590
Dongwook Kwon <tomiyadragon@gmail.com>
2021-06-21 17:50:59 +0900

    Update 0621_1.py

--- 0621_1.py
+++ 0621_1.py
@@ -21,10 +21,54 @@ print('FPS: ', fps)
 delay = int(1000 / fps)
 
 w = round(cap1.get(cv2.CAP_PROP_FRAME_WIDTH))
-h = round(cap2.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = round(cap1.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
 
 out = cv2.VideoWriter('output.avi', fourcc, fps, (w, h))
+
+for i in range(frame_cnt1 - effect_frames):
+    ret1, frame1 = cap1.read()
+    
+    if not ret1:
+        break
+    
+    out.write(frame1)
+    
+    cv2.imshow('frame', frame1)
+    cv2.waitKey(delay)
+    
+for i in range(effect_frames):
+    ret1, frame1 = cap1.read()
+    ret2, frame2 = cap2.read()
+    
+    if not ret1 or not ret2:
+        break
+    
+    dx = int(w * i / effect_frames)
+    
+    frame = np.zeros((h, w, 3), dtype = np.uint8)
+    """
+    frame[:, 0:dx, :] = frame2[:, 0:dx, :]
+    frame[:, dx:w, :] = frame1[:, dx:w, :]
+    """
+    frame[:, 0:dx, :] = frame2[:, 0:dx, :]
+    frame[:, dx:w, :] = frame1[:, dx:w, :]
+    
+    out.write(frame)
+    cv2.imshow('frame', frame)
+    cv2.waitKey(delay)
+    
+    
+for i in range(effect_frames, frame_cnt2):
+    ret2, frame2 = cap2.read()
+    
+    if not ret2:
+        break
+    
+    out.write(frame2)
+    
+    cv2.imshow('frame', frame2)
+    cv2.waitKey(delay)
 
 cap1.release()
 cap2.release()
