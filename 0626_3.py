cba13870553a7423c6e31f068ef081539f2a43e6
Dongwook Kwon <tomiyadragon@gmail.com>
2021-06-26 17:12:23 +0900

    Create 0626_3.py

---
+++ 0626_3.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 26 15:15:24 2021
+
+@author: 82106
+"""
+
+import sys
+import numpy as np
+import cv2
+
+cap1 = cv2.VideoCapture('../Data/cv_fc_data/ch03/woman.mp4')
+
+if not cap1.isOpened():
+    print('video open failed!')
+    sys.exit()
+    
+cap2 = cv2.VideoCapture('../Data/cv_fc_data/ch03/raining.mp4')
+
+if not cap2.isOpened():
+    print('video open failed!')
+    sys.exit()
+    
+w = round(cap1.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = round(cap1.get(cv2.CAP_PROP_FRAME_HEIGHT))
+frame_cnt1 = round(cap1.get(cv2.CAP_PROP_FRAME_COUNT))
+frame_cnt2 = round(cap2.get(cv2.CAP_PROP_FRAME_COUNT))
+print('w x h: {} x {}'.format(w, h))
+print('frame_cnt1: ', frame_cnt1)
+print('frame_cnt2: ', frame_cnt2)
+
+fps = cap1.get(cv2.CAP_PROP_FPS)
+delay = int(1000 / fps)
+
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+out = cv2.VideoWriter('output.avi', fourcc, fps, (w, h))
+
+do_composit = False
+
+while True:
+    ret1, frame1 = cap1.read()
+    
+    if not ret1:
+        break
+    
+    if do_composit:
+        ret2, frame2 = cap2.read()
+        
+        if not ret2:
+            berak
+            
+        hsv = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, (50, 150, 0), (80, 255, 255))
+        cv2.copyTo(frame2, mask, frame1)
+    
+    out.write(frame1)
+
+    cv2.imshow('frame', frame1)
+    key = cv2.waitKey(delay)
+    
+    if key == ord(' '):
+        do_composit = not do_composit
+    elif key == 27:
+        break
+
+cap1.release()
+cap2.release()
+out.release()
+cv2.destroyAllWindows()
+
+        
